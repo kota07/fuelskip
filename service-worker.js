a6bd7bcac9236572247a725e8a430d43d5123645
Teja <kotateja09@gmail.com>
2026-01-06 15:41:10 +0530

    production_changes

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,14 @@
-const CACHE_NAME = "fueltag-v1";
+const CACHE_NAME = "fueltag-v2";
 
 const FILES_TO_CACHE = [
+  "/",
   "/customer.html",
-  "/manifest.json"
+  "/attendant.html",
+  "/owner.html",
+  "/manifest.json",
+  "https://sdk.cashfree.com/js/v3/cashfree.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js",
+  "https://unpkg.com/jsqr@1.4.0/dist/jsQR.js"
 ];
 
 self.addEventListener("install", (event) => {
@@ -12,12 +18,28 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
+  // Stale-While-Revalidate
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          if (networkResponse.status === 200) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(() => null);
+        return response || fetchPromise;
+      });
+    })
   );
 });
